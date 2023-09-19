355ea9b7cf397e7b8803febd890c79012bcb7aa7
Salim Oyinlola <salimoyinlola@gmail.com>
2023-09-19 08:11:45 +0100

    Added second path

--- app.py
+++ app.py
@@ -1,26 +1,30 @@
 import requests, os, uuid, json
 from dotenv import load_dotenv
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
+
 load_dotenv()
 from flask import Flask, redirect, url_for, request, render_template, session
 
 app = Flask(__name__)
+
+
 
 @app.route('/')
 def index():
     # Render the HTML template
     return render_template('index.html')
     
+# Option 1 
 
 @app.route('/option/<int:option_number>', methods = ['GET'])
-def option(option_number):
+def index_one(option_number):
     # Generate the filename based on the option_number (e.g., 'index-1.html')
     filename = f'index-{option_number}.html'
     return render_template(filename)
- 
-    
 
-@app.route('/option/1/', methods=['POST'])
-def index_post():
+@app.route('/option/1', methods=['POST'])
+def index_post_one():
     # Read the values from the form
     original_text = request.form['text']
     target_language = request.form['language']
@@ -63,6 +67,56 @@ def index_post():
         original_text=original_text,
         target_language=target_language
     )
+
+
+
+# Option 2
+
+
+language_key = os.getenv("LANGUAGE_KEY")
+language_endpoint = os.getenv("LANGUAGE_ENDPOINT")
+def authenticate_client():
+    ta_credential = AzureKeyCredential(language_key)
+    text_analytics_client = TextAnalyticsClient(
+            endpoint=language_endpoint, 
+            credential=ta_credential)
+    return text_analytics_client
+
+client = authenticate_client()
+
+@app.route('/option/2', methods=['GET'])
+def index_two():
+    return render_template('index-2.html')
+
+@app.route('/option/2', methods=['POST'])
+def index_post_two():
+    # Read the values from the form
+    original_text = request.form['text']
+    documents = [original_text]
+
+
+    result = client.analyze_sentiment(documents, show_opinion_mining=True)
+    doc_result = [doc for doc in result if not doc.is_error]
+
+    positive_reviews = [doc for doc in doc_result if doc.sentiment == "positive"]
+    negative_reviews = [doc for doc in doc_result if doc.sentiment == "negative"]
+
+    positive_mined_opinions = []
+    mixed_mined_opinions = []
+    negative_mined_opinions = []
+
+    for document in doc_result:
+        sentiment = document.sentiment
+        positive_confidence_scores = document.confidence_scores.positive
+        negative_confidence_scores = document.confidence_scores.negative
+        neutral_confidence_scores = document.confidence_scores.neutral
+    return render_template(
+        'results-2.html', 
+        sentiment = sentiment, 
+        positive_confidence_scores = positive_confidence_scores,
+        negative_confidence_scores = negative_confidence_scores,
+        neutral_confidence_scores = neutral_confidence_scores
+        )
 
 
 
